66098769f16e6633a365336754238d5027610e43
Joel Estumano <joelestumano@gmail.com>
2025-07-04 14:39:57 -0300

    Feat :sparkles: Configuração do cypress para os testes e2e.

--- src/app/layout/pages/not-found-page/not-found-page.component.spec.ts
+++ src/app/layout/pages/not-found-page/not-found-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotFoundPageComponent } from './not-found-page.component';
+import { provideRouter } from '@angular/router';
 
 describe('NotFoundPageComponent', () => {
 	let component: NotFoundPageComponent;
@@ -10,7 +11,8 @@ describe('NotFoundPageComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [NotFoundPageComponent]
+			imports: [NotFoundPageComponent],
+			providers: [provideRouter([])]
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(NotFoundPageComponent);
